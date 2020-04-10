93e53479745fde3267a1590c804b559cde530c97
Jakub Zegar <jakubzegar3@gmail.com>
2020-04-10 15:16:18 +0200

    Navbar added

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,3 +1,38 @@
 <template>
-  <div>index</div>
+    <mdb-view>
+      <mdb-mask class="gradient d-flex justify-content-center align-items-center">
+        <navbar/>
+        <router-view/>
+      </mdb-mask>
+    </mdb-view>
 </template>
+
+<script>
+  import {  mdbView, mdbMask } from 'mdbvue';
+  import navbar from './Navbar';
+
+  export default {
+    name: 'app',
+    components: {
+      mdbView,
+      mdbMask,
+      navbar,
+    }
+  }
+</script>
+
+<style scoped>
+
+  .view {
+    background-image: url('https://mdbootstrap.com/img/Photos/Others/architecture.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center center;
+    height: 100vh;
+  }
+  .gradient {
+    background: -moz-linear-gradient(45deg, rgba(42, 27, 161, 0.7), rgba(29, 210, 177, 0.7) 100%);
+  }
+  
+</style>
